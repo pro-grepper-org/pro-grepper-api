981d9d9886d1a679ccc95264501b4d81ad2c0053
Yash Tailor <yashtailor2000@gmail.com>
2021-05-02 16:19:50 +0530

    added cross origin support

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask,request,jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/api/companies',methods = ['POST'])
 def get_company_tags():
